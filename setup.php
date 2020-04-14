1fe901de9fb35d488bcc8cc7fc15eee51d712397
tomolimo <olivier.moron@araymond.com>
2020-04-14 17:07:30 +0200

    Fixes a bug on extension comparison (compare extension in lower cases) Set version to 1.1.6

--- setup.php
+++ setup.php
@@ -30,7 +30,7 @@ along with GLPI. If not, see <http://www.gnu.org/licenses/>.
 // Original Author of file: Olivier Moron
 // Purpose of file: to setup office online plugin to GLPI
 // ----------------------------------------------------------------------
-define ("PLUGIN_OFFICEONLINE_VERSION", "1.1.5");
+define ("PLUGIN_OFFICEONLINE_VERSION", "1.1.6");
 /**
  * Summary of plugin_init_officeonline
  */
